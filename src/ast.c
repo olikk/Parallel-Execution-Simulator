cf4ebaa186643b1bbd340abd5457e03417c20af3
Olga Pigareva <olivka@MacBook-Pro-de-Olga-3.local>
2019-02-22 00:06:25 +0100

    more ast nodes

--- src/ast.c
+++ src/ast.c
@@ -3,12 +3,57 @@
 #include <string.h>
 #include "ast.h"
 
+#include <assert.h>
+
+static void* check_alloc(size_t sz)
+{
+    void* result = calloc(sz, 1);
+    if(!result)
+    {
+        fprintf(stderr, "alloc failed\n");
+        exit(1);
+    }
+}
+
 ast* ast_new_operation(char* op, ast* left, ast* right) {
-  ast* new = malloc(sizeof(ast));
-  new->type = strdup(op);
+
+  ast* new = check_alloc(sizeof(ast));
+
+  new->type = operation_type;
+  new->u.operation.op = strdup(op);
   new->u.operation.left = left;
   new->u.operation.right = right;
+
   return new;
+
+}
+
+ast* ast_new_assignement(char* name, ast* right){
+
+  ast* new = malloc(sizeof(ast));
+
+  new->type = assignement_type;
+  new->u.assignement.name = name;
+  new->u.assignement.right = right;
+
+  return new;
+}
+
+ast* ast_new_statements(int, ast*);
+
+ast* ast_new_while(ast*, ast*);
+
+ast* ast_new_if(ast* cond, ast* if_branch, ast* else_branch){
+
+  ast* new = malloc(sizeof(ast));
+
+  new->type = if_type;
+  new->u.if_stmt.cond = cond;
+  new->u.if_stmt.right.if_branch = if_branch;
+  new->u.if_stmt.right.else_branch = else_branch;
+  
+  return new;
+
 }
 
 ast* ast_new_number(int number) {
--- src/ast.h
+++ src/ast.h
@@ -1,16 +1,58 @@
 typedef struct ast {
-  char* type;
+
+  enum {id_type, number_type, operation_type, assignment_type, while_type, statements_type, if_type, last_element} type;
+
   union {
-    struct {
-      struct ast* left;
-      struct ast* right;
-    } operation;
+
     int number;
     char* id;
+
+    struct {
+
+      struct ast* left;
+      struct ast* right;
+
+      char op;
+
+    } operation;
+
+    struct {
+
+      char*name;
+      struct ast* right;
+
+    } assignment;
+
+     struct {
+
+      int count;
+      struct ast** statements;
+
+      } statements;
+
+      struct {
+        struct ast* cond;
+        struct ast* statements;
+
+      } while_stmt;
+
+      struct {            // for "if/else" statements
+
+        struct ast* condition;
+        struct ast* if_branch;
+        struct ast* else_branch;
+
+      } if_stmt;
+
   } u;
+
 } ast;
 
 ast* ast_new_operation(char*, ast*, ast*);
+ast* ast_new_assignement(char*, ast*);
+ast* ast_new_statements(int, ast*);
+ast* ast_new_while(ast*, ast*);
+ast* ast_new_if(ast*, ast*, ast*);
 ast* ast_new_number(int);
 ast* ast_new_id(char*);
 void ast_print(ast*, int);
